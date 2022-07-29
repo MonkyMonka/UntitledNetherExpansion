ad56ee93cc86e6228f28b8d7b665b068486a8135
MonkyMonka <daniyaldanish11dd@gmail.com>
2022-07-29 14:26:00 -0400

    Setup for future advanced properties for magnetite

---
+++ src/main/java/com/monky/untitlednetherexpansion/block/MagnetiteBlock.java
@@ -0,0 +1,9 @@
+package com.monky.untitlednetherexpansion.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class MagnetiteBlock extends Block {
+    public MagnetiteBlock(Properties properties) {
+        super(properties);
+    }
+}
