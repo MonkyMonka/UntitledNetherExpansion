34c371548a0db2645f193637c9690972317bd963
MonkyMonka <daniyaldanish11dd@gmail.com>
2022-08-26 11:54:11 -0400

    Placeholder textures

---
+++ src/main/java/com/monky/untitlednetherexpansion/block/PorousRockBlock.java
@@ -0,0 +1,9 @@
+package com.monky.untitlednetherexpansion.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class PorousRockBlock extends Block {
+    public PorousRockBlock(Properties properties) {
+        super(properties);
+    }
+}
