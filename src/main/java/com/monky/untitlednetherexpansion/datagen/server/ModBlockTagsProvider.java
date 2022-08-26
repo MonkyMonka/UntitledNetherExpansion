4af8bed326a840c084cf47371572f3e8e86f66dd
MonkyMonka <daniyaldanish11dd@gmail.com>
2022-08-26 11:56:04 -0400

    Mineable block tags

--- src/main/java/com/monky/untitlednetherexpansion/datagen/server/ModBlockTagsProvider.java
+++ src/main/java/com/monky/untitlednetherexpansion/datagen/server/ModBlockTagsProvider.java
@@ -3,10 +3,15 @@ package com.monky.untitlednetherexpansion.datagen.server;
 import com.monky.untitlednetherexpansion.UntitledNetherExpansion;
 import com.monky.untitlednetherexpansion.init.BlockInit;
 import com.monky.untitlednetherexpansion.init.TagInit;
+import net.minecraft.client.Minecraft;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.Tag;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
+import static net.minecraft.tags.BlockTags.*;
 
 public class ModBlockTagsProvider extends BlockTagsProvider {
 
@@ -17,5 +22,28 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
     @Override
     protected void addTags() {
         tag(TagInit.Blocks.COOL_BLOCKS).add(Blocks.ACACIA_LEAVES).add(BlockInit.WHISKER_BLOCK.get());
+
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.POROUS_ROCK_BLOCK.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.MAGNETITE_BLOCK.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.MAGNETITE_BRICK_BLOCK.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.MAGNETITE_BRICK_SLAB.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.MAGNETITE_BRICK_STAIR.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.POLISHED_MAGNETITE_BLOCK.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.POLISHED_MAGNETITE_SLAB.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.POLISHED_MAGNETITE_STAIR.get());
+        tag(MINEABLE_WITH_PICKAXE).add(BlockInit.CHISELED_MAGNETITE_BLOCK.get());
+
+        tag(NEEDS_IRON_TOOL).add(BlockInit.MAGNETITE_BLOCK.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.MAGNETITE_BRICK_BLOCK.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.MAGNETITE_BRICK_SLAB.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.MAGNETITE_BRICK_STAIR.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.POLISHED_MAGNETITE_BLOCK.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.POLISHED_MAGNETITE_SLAB.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.POLISHED_MAGNETITE_STAIR.get());
+        tag(NEEDS_IRON_TOOL).add(BlockInit.CHISELED_MAGNETITE_BLOCK.get());
+
+        tag(NEEDS_STONE_TOOL).add(BlockInit.POROUS_ROCK_BLOCK.get());
+
+
     }
 }
