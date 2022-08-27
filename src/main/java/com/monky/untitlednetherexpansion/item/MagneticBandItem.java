fe07f835e1b4935e148bc4cc0ebc1e48cef077e3
MonkyMonka <daniyaldanish11dd@gmail.com>
2022-08-26 23:41:06 -0400

    Repair Functionality for Magnetic Band

--- src/main/java/com/monky/untitlednetherexpansion/item/MagneticBandItem.java
+++ src/main/java/com/monky/untitlednetherexpansion/item/MagneticBandItem.java
@@ -1,10 +1,17 @@
 package com.monky.untitlednetherexpansion.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 public class MagneticBandItem extends Item {
     public MagneticBandItem(Properties properties) {
     super(properties);
     }
 
+    @Override
+    public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
+        return repair.is(Items.NETHERITE_SCRAP);
+    }
 }
+
