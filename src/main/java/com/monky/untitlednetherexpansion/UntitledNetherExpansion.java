8ae8351a6eb3d9a9e8605920243f3ce400e6ab92
MonkyMonka <daniyaldanish11dd@gmail.com>
2022-08-27 22:50:32 -0400

    Animated Item model and predicate

--- src/main/java/com/monky/untitlednetherexpansion/UntitledNetherExpansion.java
+++ src/main/java/com/monky/untitlednetherexpansion/UntitledNetherExpansion.java
@@ -3,6 +3,8 @@ package com.monky.untitlednetherexpansion;
 import com.monky.untitlednetherexpansion.init.BlockInit;
 import com.monky.untitlednetherexpansion.init.ItemInit;
 import com.monky.untitlednetherexpansion.init.SoundEventsInit;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -10,9 +12,12 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import static net.minecraft.world.item.CompassItem.isLodestoneCompass;
 
 @Mod("untitlednetherexpansion")
 public class UntitledNetherExpansion {
@@ -33,10 +38,27 @@ public class UntitledNetherExpansion {
     public UntitledNetherExpansion() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        bus.addListener(this::setup);
+
         ItemInit.ITEMS.register(bus);
         BlockInit.BLOCKS.register(bus);
         SoundEventsInit.SOUNDS.register(bus);
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLClientSetupEvent event)
+    {
+        event.enqueueWork(() ->
+        {
+            ItemProperties.register(ItemInit.WAYFINDER_ITEM.get(),
+                    new ResourceLocation(UntitledNetherExpansion.MOD_ID, "attunement"), (stack, level, living, id)  -> {
+                        if (isLodestoneCompass(stack)) {
+                            return 1.0F;
+                        } else {
+                            return 0.0F;
+                        }
+                    });
+                    });
     }
 }
